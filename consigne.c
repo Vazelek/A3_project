690b7edf13914aac301a90eee1af2942db352954
Bastien Huard <bastien.huard@isen-ouest.yncrea.fr>
2023-03-20 10:57:50 +0100

    function to read consigne

--- consigne.c
+++ consigne.c
@@ -1,11 +1,27 @@
 #include "consigne.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-   float consigne(float thermostatPrec_f)
-   {
-   
-      float thermostat_f=0;
-   
-     
-      return thermostat_f;
-   
-   }
+float consigne(float thermostatPrec_f)
+{
+    float thermostat_f;
+    FILE* ptr;
+    char * cons = (char *) malloc( 4 );
+    if (access(".verrouConsigne", F_OK) == 0) {
+        thermostat_f=thermostatPrec_f;
+    } else {
+        fopen (".verrouConsigne", "r");
+        ptr = fopen("consigne.txt", "r");
+        if (NULL == ptr) {
+            printf("file can't be opened \n");
+        }
+        fgets(cons, 4, ptr);
+        thermostat_f = atoll(cons);
+        remove (".verrouConsigne");
+    }
+    return thermostat_f;
+
+}
+
+
