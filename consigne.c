17a0705b6dd1fe469a7ddcde540a3e4451a02b80
Bastien Huard <bastien.huard@isen-ouest.yncrea.fr>
2023-03-20 11:14:08 +0100

    fix .verrouConsigne

--- consigne.c
+++ consigne.c
@@ -8,16 +8,18 @@ float consigne(float thermostatPrec_f)
     float thermostat_f;
     FILE* ptr;
     char * cons = (char *) malloc( 4 );
+
     if (access(".verrouConsigne", F_OK) == 0) {
         thermostat_f=thermostatPrec_f;
     } else {
-        fopen (".verrouConsigne", "r");
+        FILE* fileptr = fopen(".verrouConsigne","wx");
         ptr = fopen("consigne.txt", "r");
         if (NULL == ptr) {
             printf("file can't be opened \n");
         }
         fgets(cons, 4, ptr);
         thermostat_f = atoll(cons);
+        fclose(fileptr);
         remove (".verrouConsigne");
     }
     return thermostat_f;
