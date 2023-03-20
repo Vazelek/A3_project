409e52d1c5416ab638c5e47065185ff9bd5d756c
Bastien Huard <bastien.huard@isen-ouest.yncrea.fr>
2023-03-20 13:09:25 +0100

    fix main

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
-#include "autotests.h"
-#include "consigne.h"
 #include "define.h"
+#include "consigne.h"
 #include "regulation.h"
 #include "simulateur.h"
 #include "visualisationC.h"
 #include "visualisationT.h"
+#include "autotests.h"
 
 int main (int argc, char** argv){
 
