8821fa693ac6b78e0b408694a774c37a82ebaa56
Vazelek <valentinherve60@gmail.com>
2023-03-20 10:06:08 +0100

    Add ifndef in header

--- regulation.h
+++ regulation.h
@@ -1,7 +1,10 @@
-    #include <stdio.h>
-	#include <stdlib.h>
-	#include "define.h" 
-    
-   // Pensez à bien proteger vos .h avec des ifndef
+#ifndef REGULATION_H
+#define REGULATION_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "define.h" 
 	
-   float regulationTest(int regul,float consigne,float* tabT, int nT);
+float regulationTest(int regul,float consigne,float* tabT, int nT);
+
+#endif
