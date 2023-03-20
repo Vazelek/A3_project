c6309543cc09777da0ae144d10c4b5cf2e013fcc
Vazelek <valentinherve60@gmail.com>
2023-03-20 13:41:35 +0100

    Remove useless printf

--- visualisationT.c
+++ visualisationT.c
@@ -29,26 +29,13 @@ void visualisationT(temp_t myTemp)
     unsigned short int temoin = 0;
     fgets(str, 8, fileptr);
     temoin = (strcmp(str, "true\r\n") == 0); // strcmp = 0 if str == "true\n"
-    printf("%s\n", temoin ? "true" : "false");
-    printf("%d\n", temoin);
     fgets(str, 8, fileptr);
     myTemp.interieure = atof(str);
-    printf("%f\n", myTemp.interieure);
     fgets(str, 8, fileptr);
     myTemp.exterieure = atof(str);
-    printf("%f\n", myTemp.exterieure);
 
     fclose(fileptr);
 
     remove(".verrouData");
 
 }
-/*
-int main(){
-    temp_t myTemp;
-    myTemp.exterieure = 15.0;
-    myTemp.interieure = 20.0;
-    visualisationT(myTemp);
-    return 0;
-}
- */
