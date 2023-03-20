fa7f216e5d30f0056e5085fd5df5fb0c740a6a6e
Bastien Huard <bastien.huard@isen-ouest.yncrea.fr>
2023-03-20 14:31:13 +0100

    begin regulation function

--- regulation.c
+++ regulation.c
@@ -1,6 +1,15 @@
     #include <stdio.h>
 	 #include <stdlib.h>
     #include "regulation.h"
+
+	float regulation(int type){
+        if (type==1){
+            return 50;
+        }
+        else{
+            
+        }
+}
 	 
 	float regulationTest(int regul,float consigne,float* tabT, int nT){
 		float cmd;
