e43aa944961534b7d946d08b785195607549a048
Vazelek <valentinherve60@gmail.com>
2023-03-24 14:51:47 +0100

    comment test_usb

--- commande.c
+++ commande.c
@@ -5,9 +5,11 @@ FT_STATUS write_puis(FT_HANDLE ftHandle, float puissance){
     DWORD BytesWritten;
     char TxBuffer[1] = {0}; // Contains data to write to device
 
+    // puissance \in [0, 100] and must be send in [0, 127]
+    // MSB is 1 if the eater is ON (puissance > 0), 0 otherwise
     TxBuffer[0] = ((unsigned char) ((puissance * 127) / 100)) | ((puissance != 0) << 7);
 
-    return FT_Write(ftHandle, TxBuffer, sizeof(TxBuffer), &BytesWritten);
+    return FT_Write(ftHandle, TxBuffer, sizeof(TxBuffer), &BytesWritten); // send data to the card and return status
 }
 
 
--- test_usb.c
+++ test_usb.c
@@ -11,6 +11,7 @@ int main(){
 
 	printf("Test USB\n\n");
 
+    // delete verrou files if exists
     if(access(".verrouData", F_OK) == 0) { // file exists
         remove(".verrouData");
     }
@@ -21,7 +22,7 @@ int main(){
     FT_HANDLE ftHandle;
     FT_STATUS ftStatus;
 
-    ftStatus = FT_Open(0, &ftHandle);
+    ftStatus = FT_Open(0, &ftHandle); // Open connection between computer and card
     if(ftStatus != FT_OK) {
         printf("FT_Open failed\n");
         return 1;
@@ -42,37 +43,37 @@ int main(){
         printf("FT_SetFlowControl failed\n");
         return 1;
     }
-    ftStatus = FT_SetTimeouts(ftHandle, 40, 0);
+    ftStatus = FT_SetTimeouts(ftHandle, 40, 0); // set timeout to 40 ms
     if(ftStatus != FT_OK) {
         printf("FT_SetTimeouts failed\n");
         return 1;
     }
 
-    float cons = consigne(10);
-    temp_t temperatures;
+    float cons = consigne(10); // actual instruction
+    temp_t temperatures; // indoor and outdoor temperature
 
-    unsigned short int regul = 2;
-	float tint_old = 15;
-	float tint = 15;
-	float cons_old = cons;
-	float puissance = 50;
-	unsigned char premiere_iteration = 1;
-	float pid_i_old = 0;
+    unsigned short int regul = 2; // Regulation method : 1 => TOR, other than 1 => PID
+	float tint_old = 15; // previous temperature 
+	float tint = 15; // actual temperature
+	float cons_old = cons; // previous instruction
+	float puissance = 50; // command
+	unsigned char premiere_iteration = 1; // 1 if it is the first iteration in while loop
+	float pid_i_old = 0; // air under the curve (integral)
 
-    while(cons > 5){
-        cons_old = cons;
-        cons = consigne(cons);
-        tint_old = tint;
-        ftStatus = read_temp(ftHandle, &temperatures);
+    while(cons > 5){ // stop if the instruction is under 5°C
+        cons_old = cons; // copy the actual instruction inside the previous one
+        cons = consigne(cons); // read consigne.txt file to get the instruction
+        tint_old = tint; // copy actual indoor temperature inside the previous one
+        ftStatus = read_temp(ftHandle, &temperatures); // read the temperatures sent by the card via USB
         if(ftStatus != FT_OK) {
             printf("read_temp failed\n");
             return 1;
         }
-        tint = temperatures.interieure;
-        puissance = regulation_Global(regul, cons, tint, cons_old, tint_old, &pid_i_old, premiere_iteration);
-        visualisationC(puissance);
-        visualisationT(temperatures);
-        ftStatus = write_puis(ftHandle, puissance);
+        tint = temperatures.interieure; // copy the indoor temperature inside tint
+        puissance = regulation_Global(regul, cons, tint, cons_old, tint_old, &pid_i_old, premiere_iteration); // calculate the command according to the actual and previous temperature and instruction
+        visualisationC(puissance); // update "true" or "false" for the heater in data.txt
+        visualisationT(temperatures); // update indoor and outdoor temperature in data.txt
+        ftStatus = write_puis(ftHandle, puissance); // send puis to the card via USB
         if(ftStatus != FT_OK) {
             printf("write_puis failed\n");
             return 1;
@@ -81,7 +82,7 @@ int main(){
             premiere_iteration = 0;
         }
 
-        Sleep(40);
+        Sleep(40); // wait 40 ms before another loop (synchronize with the card)
     }
     FT_Close(ftHandle);
     return 0;
