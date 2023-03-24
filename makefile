9ccbc777bb52477615b255c17674ae73476b8d9a
Vazelek <valentinherve60@gmail.com>
2023-03-24 09:35:00 +0100

    update makefile and add diff main

--- makefile
+++ makefile
@@ -1,2 +1,15 @@
-main: autotests.c consigne.c main.c regulation.c simulateur.c test_simulateur.c visualisationC.c visualisationT.c releve.c commande.c test_usb.c ftd2xx.lib
-	gcc autotests.c consigne.c main.c regulation.c simulateur.c test_simulateur.c visualisationC.c visualisationT.c releve.c commande.c test_usb.c ftd2xx.lib -o test -Wall
+# main: autotests.c consigne.c main.c regulation.c simulateur.c test_simulateur.c visualisationC.c visualisationT.c releve.c commande.c test_usb.c ftd2xx.lib
+# 	gcc autotests.c consigne.c main.c regulation.c simulateur.c test_simulateur.c visualisationC.c visualisationT.c releve.c commande.c test_usb.c ftd2xx.lib -o test -Wall
+
+autotests: autotests.c consigne.c regulation.c test_autotest.c visualisationC.c visualisationT.c 
+	gcc -c autotests.c consigne.c regulation.c test_autotest.c visualisationC.c visualisationT.c 
+	gcc autotests.o consigne.o regulation.o test_autotest.o visualisationC.o visualisationT.o -o autotests -Wall
+	rm -rf *.o
+
+simu: consigne.c regulation.c test_simulateur.c visualisationC.c visualisationT.c simulateur.c
+	gcc -c consigne.c regulation.c test_simulateur.c visualisationC.c visualisationT.c simulateur.c
+	gcc consigne.o regulation.o test_simulateur.o visualisationC.o visualisationT.o simulateur.o ftd2xx.dll -o simu -Wall
+	rm -rf *.o
+
+clean:
+	rm -rf *.o *.exe
--- test_simulateur.c
+++ test_simulateur.c
@@ -1,19 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include "define.h"
+#include "consigne.h"
+#include "regulation.h"
 #include "simulateur.h"
-/*
+#include "visualisationC.h"
+#include "visualisationT.h"
+
 int main(){
+
+	// May be done by make
+	if(access(".verrouData", F_OK) == 0) { // file exists
+        remove(".verrouData");
+    }
+	if(access(".verrouConsigne", F_OK) == 0) { // file exists
+        remove(".verrouConsigne");
+    }
 
 	temp_t temperature;
 	temperature.exterieure = 14.0;
-	temperature.interieure = 15.0;
-	struct simParam_s*  monSimulateur_ps = simConstruct(temperature); // creation du simulateur, puissance intialis�e � 0%
-	int i=0; // increment de boucle
-	float puissance = 70.0; // puissance de chauffage
-	for(i=0;i< 30;i++){
-		temperature=simCalc(puissance,monSimulateur_ps); // simulation de l'environnement
+	temperature.interieure = 30.0;
+	struct simParam_s*  sim_params = simConstruct(temperature);
+
+	int i = 0;
+	unsigned short int regul = 2;
+	float cmd = 10;
+	float puissance = 50;
+	float tabT[10000];
+	while(cmd = 5 && i < 10000){
+		tabT[i] = temperature.interieure;
+		visualisationT(temperature);
+		cmd = consigne(cmd);
+		puissance = regulationTest(regul, cmd, tabT, i + 1);
+		sim_params->tempExt_f = temperature.exterieure;
+		sim_params->tempInt_f = temperature.interieure;
+		temperature=simCalc(puissance, sim_params);
+		visualisationC(puissance);
+		i++;
 	}
-	simDestruct(monSimulateur_ps); // destruction de simulateur
+
+	simDestruct(sim_params); // destruction de simulateur
 	return EXIT_SUCCESS;
+    
 }
- */
